1b433fe11df08a7e849b7e53ed04f6666c0d817a
Bjørn A. Johansen <baj-@outlook.com>
2023-09-08 16:33:40 +0200

    better checks for already existing links and commands

--- script/osx.sh
+++ script/osx.sh
@@ -1,25 +1,28 @@
 source script/log.sh
 
 function setup_osx() {
-  if [ -f /usr/local/bin/gh ]; then
-    version=$(gh version)
-    note "GitHub CLI ${version} is already installed"
-  else
+  if ! command -v gh &> /dev/null
+  then
     info "Installing GitHub CLI..."
     # Install GitHub CLI
     brew install gh
     version=$(gh version)
     info "GitHub CLI ${version} installed"
+  else
+    note "$(gh version | grep "gh version") is already installed"
   fi
 
-  info "Install ripgrep..."
-  brew install ripgrep
-  info "ripgrep installed"
-  
-  if [ -f /usr/local/bin/nvim ]; then
-    version=$(nvim --version | grep "NVIM")
-    note "${version} is already installed"
+  if ! command -v rg &> /dev/null
+  then
+    info "Install ripgrep..."
+    brew install ripgrep
+    info "ripgrep installed"
   else
+    note "$(rg --version | grep ripgrep) is already installed"
+  fi
+
+  if ! command -v nvim &> /dev/null
+  then
     info "Installing neovim..."
     brew install neovim
     info "neovim installed"
@@ -31,12 +34,12 @@ function setup_osx() {
     mkdir -p "${HOME}/.config/nvim"
     info "Install Plug"
     curl -fLo "${HOME}/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  else
+    note "$(nvim --version | grep "NVIM") is already installed"
   fi
 
-  if [ -f /usr/local/bin/starship ]; then
-    version=$(starship --version | grep "starship")
-    note "${version} is already installed"
-  else
+  if ! command -v starship &> /dev/null
+  then
     info "Installing starship..."
     brew install starship
     info "Starship installed"
@@ -44,22 +47,17 @@ function setup_osx() {
     info "Symlink Starship config"
     ln -sf "${DOTFILES_DIR}/dotfiles/config/starship.toml" "${HOME}/.config/starship.toml"
 
-    info "Installing Nerd Font"
-    brew tap homebrew/cask-fonts
-    brew install --cask font-hack-nerd-font
-
-    info "Installing iTerm2 shell integration"
-    curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
-    note "Open iterm2 profiles > Text and select Hack Nerd Font"
+  else
+    note "$(starship --version | grep "starship") is already installed"
   fi
 
-  if [ -f /usr/local/bin/tig ]; then
-    version=$(tig --version | grep "tig")
-    note "${version} is already installed"
-  else
+  if ! command -v tig &> /dev/null
+  then
     info "Installing tig..."
     brew install tig
     info "tig installed"
+  else
+    note "$(tig --version | grep "tig") is already installed"
   fi
 
   info "Symlink .zshrc"
@@ -68,16 +66,44 @@ function setup_osx() {
   info "Symlink z folder jumper"
   ln -sf "${DOTFILES_DIR}/dotfiles/z.sh" "${HOME}/z.sh"
 
-  info "Installing mercurial for gvm..."
-  brew install mercurial
+  if ! command -v hg &> /dev/null
+  then
+    info "Installing mercurial for gvm..."
+    brew install mercurial
+  else
+    note "$(hg --version | grep "Mercurial") is already installed"
+  fi
 
-  info "Installing go using brew to get around gvm bootstrapping problem..."
-  brew install go
+  if ! command -v go &> /dev/null
+  then
+    info "Installing go using brew to get around gvm bootstrapping problem..."
+    brew install go
+  else
+    note "$(go version) is already installed"
+  fi
 
   info "Installing tokyonight-storm color theme for iTerm"
-  curl -o "${HOME}/Documents" https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/tokyonight-storm.itermcolors
+  curl -o "${HOME}/Downloads" https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/tokyonight-storm.itermcolors
+
+  if ! [ -f "${HOME}/Library/Fonts/HackNerdFont-Regular.ttf" ]
+  then
+    info "Installing Nerd Font"
+    brew tap homebrew/cask-fonts
+    brew install --cask font-hack-nerd-font
+  else
+    note "Nerd Font already installed"
+  fi
+
+  if ! [ -f "${HOME}/.iterm2_shell_integration.zsh" ]
+  then
+    info "Installing iTerm2 shell integration"
+    curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
+  else
+    note "iTerm2 Shell Integration is already installed"
+  fi
 
   echo ""
+  note "Open iterm2 profiles > Text and select Hack Nerd Font"
   note "*** --->>> Import iTerm2 settings from Dropbox <<<--- ***"
   echo ""
 }
--- setup.sh
+++ setup.sh
@@ -9,26 +9,40 @@ mkdir -p "${HOME}/.config"
 DOTFILES_DIR=$(cd $(dirname "$0")/.. && pwd)
 info "Dotfiles directory: ${DOTFILES_DIR}"
 
-info "Installing nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-info "nvm installed"
+# nvm is not loaded into this environment
+if ! [ -f "${HOME}/.nvm/nvm.sh" ]
+then
+  info "Installing nvm"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  info "nvm installed"
+else
+  # load nvm
+  . $HOME/.nvm/nvm.sh
+  note "nvm $(nvm --version) is already installed"
+fi
 
-info "Installing gvm"
-zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-info "gvm installed"
+if ! command -v gvm &> /dev/null
+then
+  info "Installing gvm"
+  zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+  info "gvm installed"
+else
+  note "$(gvm version)"
+fi
 
 info "Symlink init.lua"
 ln -sf "${DOTFILES_DIR}/dotfiles/init.lua" "${HOME}/.config/nvim/init.lua"
 info "Symlink lua/"
-ln -sn "${DOTFILES_DIR}/dotfiles/lua" "${HOME}/.config/nvim/lua"
+ln -sfn "${DOTFILES_DIR}/dotfiles/lua" "${HOME}/.config/nvim/lua"
 info "Symlink .vimrc"
 ln -sf "${DOTFILES_DIR}/dotfiles/vimrc" "${HOME}/.vimrc"
 
-if [ $CODESPACES ]; then
+if [ $CODESPACES ]
+then
   setup_linux
 fi
 
-if [[ $OSTYPE == "darwin"* ]]; then
+if [[ $OSTYPE == "darwin"* ]]
+then
   setup_osx
 fi
-
