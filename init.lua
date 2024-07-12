0ae1573fee98ac89e254dc248d37a4213fb01f6c
Bjorn Johansen <baj-@outlook.com>
2024-07-12 09:39:59 +0200

    ignore vendor folder (Go)

--- init.lua
+++ init.lua
@@ -47,7 +47,7 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-require('telescope').setup{  defaults = { file_ignore_patterns = { "node_modules" }} }
+require('telescope').setup{  defaults = { file_ignore_patterns = { "node_modules", "vendor" }} }
 
 -- DEBUGGING ---
 -- Run setup for Mason
