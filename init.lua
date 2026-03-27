35457539b9599b10f81d1579ad34321c49234d0e
Bjorn Johansen <baj-@outlook.com>
2026-03-27 13:04:10 +0000

    Add ALE linting status indicator

--- init.lua
+++ init.lua
@@ -134,6 +134,17 @@ end
 -- Set colorscheme
 vim.cmd.colorscheme(colorSchemeName)
 
+-- ALE linting status indicator
+vim.g.ale_linting = false
+vim.api.nvim_create_autocmd("User", {
+  pattern = "ALELintPre",
+  callback = function() vim.g.ale_linting = true; vim.cmd("redrawstatus") end,
+})
+vim.api.nvim_create_autocmd("User", {
+  pattern = "ALELintPost",
+  callback = function() vim.g.ale_linting = false; vim.cmd("redrawstatus") end,
+})
+
 -- Statusline
 local function git_branch()
     local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
@@ -151,6 +162,7 @@ local function statusline()
     local file_name = " %f"
     local modified = "%m"
     local align_right = "%="
+    local lint_status = vim.g.ale_linting and " [linting...] " or ""
     local fileencoding = " %{&fileencoding?&fileencoding:&encoding}"
     local fileformat = " [%{&fileformat}]"
     local filetype = " %y"
@@ -158,13 +170,14 @@ local function statusline()
     local linecol = " %l:%c"
 
     return string.format(
-        "%s %s %s%s%s%s%s%s%s%s%s",
+        "%s %s %s%s%s%s%s%s%s%s%s%s",
         set_color_1,
         branch,
         set_color_2,
         file_name,
         modified,
         align_right,
+        lint_status,
         filetype,
         fileencoding,
         fileformat,
