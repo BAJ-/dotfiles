04b1e31c31178e040a56f064b956872fbfea29b0
Bjorn Johansen <baj-@outlook.com>
2026-03-27 16:48:07 +0000

    let treesitter handle indentation

--- init.lua
+++ init.lua
@@ -96,6 +96,9 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  indent = {
+    enable = true,
+  },
 }
 
 local telescope = require('telescope')
